1507284ee332e77a693614d27a66e64eb893e778
Abubakr Elzoghby <abubakr.z@gmail.com>
2020-08-08 03:21:18 +0300

    remove unused array

--- src/Ofe/config.php
+++ src/Ofe/config.php
@@ -1,12 +1,5 @@
 <?php
 
-$dirs = [
-	'inc' => 'inc',
-	'logic' => defined(logicDir)?:'logic',
-	'views' => defined(viewsDir)?:'views',
-	'layout' => defined(layoutDir)?:'layout',
-];
-
 define('inc' , __DIR__ . DS . 'inc' . DS);
 define('logic' , logicDir);
 define('views' , viewsDir);
